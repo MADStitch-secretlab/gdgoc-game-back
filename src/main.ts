d49ad27aa293858c301766641a82ca409a5603d9
MADStitch-secretlab <limdonghyuk12@gmail.com>
2025-10-31 14:32:39 +0900

    third

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: [
+      'http://localhost:5000',
+      'https://port-next-gdgoc-game-front-m4h5z3nf13944b7f.sel4.cloudtype.app'
+
+    ]
+  });
+  await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
